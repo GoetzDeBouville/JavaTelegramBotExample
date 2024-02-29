3f777825891e8d18ea6663e135a31dcd75a4bcb6
GoetzDeBouville <count.of.tuscan@gmail.com>
2024-03-01 01:49:08 +0300

    add comments

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -9,33 +9,64 @@ import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
 
     public static void main(String[] args) {
+        /**
+         * создадим экземпляр класса TelegramLongPollingBot и сразу проинициализуем его в соответствии с документацией.
+         * Класс TelegramLongPollingBot имеет несколько констркуторов, но мы воспользуемся констркутором, который
+         * принимает токен в виде строки.
+         * Токен передаем в виде поля класса BotConfig, где константа TOKEN является статической
+         */
         TelegramLongPollingBot myBot = new TelegramLongPollingBot(BotConfig.TOKEN) {
             @Override
-            public void onUpdateReceived(Update update) {
-                if (update != null && update.hasMessage()) {
+            public void onUpdateReceived(Update update) { // переопределение метода onUpdateReceived
+                if (update != null && update.hasMessage()) { // проверка что
+                    /**
+                     * бот работает по принципу: получил сообзение - выполнил команду.
+                     * Наш бот будет преобразовывать текст сообщения и возвращать его пользователю.
+                     * Чтобы работать с сообщением, необходимо получить его инстанс. для этого создаем экземпляр класса Message,
+                     * и положим в него сообщение, которое возвращает метод getMessage класса Update,
+                     * который в свою очередь уже приходит в наш метод onUpdateReceived
+                     */
                     Message newMessage = update.getMessage();
-                    long chatId = newMessage.getChatId();
+                    long chatId = newMessage.getChatId(); // получаем id чата, чтобы бот вернул  сообщение в этот же чат
+                    /**
+                     * Создаем экземпляр нашего класса MessageGenerator с конструктором MessageGenerator(String messageText, long messageId)
+                     */
                     MessageGenerator messageGenerator = new MessageGenerator(newMessage.getText(), newMessage.getMessageId());
-                    String responseText = messageGenerator.getMessage();
+                    /**
+                     * Создаем экземпляр класса String, которому присвоим строку, которую возвращает метод getMessage нашего класса MessageGenerator.
+                     * Эту строку конечно лучше переписать так String responseText = messageGenerator.getMessage();
+                     */
+                    String responseText = new String(messageGenerator.getMessage());
 
+                    // вызов метода для отправления сообзения в наш чат
                     setNotification(chatId, responseText);
                 }
             }
 
             @Override
-            public String getBotUsername() {
-                return BotConfig.BOT_NAME; // Возвращаем имя бота
+            public String getBotUsername() { // переопределение метода getBotUsername
+                return BotConfig.BOT_NAME;
             }
 
+            /**
+             * В целях декомпозиции вынесем логику отправления сообщений в отдельный метод setNotification
+             */
+
             private void setNotification(long chatId, String responseText) {
+                /**
+                 * За отправление сообщение в Telegram отвечает класс SendMessage
+                 * Создаем экземпляр класса SendMessage с конструктором SendMessage(@NonNull String chatId, @NonNull String text)
+                 */
                 SendMessage responseMessage = new SendMessage(String.valueOf(chatId), responseText);
+                /**
+                 * На экземпляре класса SendMessage вызовим метод enableMarkdownV2, этот метод отвечает за форматирование текста
+                 */
                 responseMessage.enableMarkdownV2(true);
                 try {
+                    // метод execute выполняет действие непосредственной отправки сообщения, принимает на вход объект responseMessage
                     execute(responseMessage);
                 } catch (TelegramApiException e) {
                     throw new RuntimeException(e);
@@ -45,8 +76,9 @@ public class Main {
 
 
         try {
+            // Создаем экземпляр класса TelegramBotsApi, для вызова регистрации бота
             TelegramBotsApi botsApi = new TelegramBotsApi(DefaultBotSession.class);
-            botsApi.registerBot(myBot);
+            botsApi.registerBot(myBot); // регистрируем бота вызовом метода registerBot на экземпляре класса TelegramBotsApi
         } catch (TelegramApiException e) {
             e.printStackTrace();
         }
--- src/main/java/org/example/MessageGenerator.java
+++ src/main/java/org/example/MessageGenerator.java
@@ -2,24 +2,27 @@ package org.example;
 
 import java.util.Random;
 
-public class MessageGenerator {
-    static String messageText = "this message id is ";
+public class MessageGenerator { // Создаем класс с несколькими конструкторами
+    static String messageText = "this message id is "; // поле c дефолтным значением
 
-    MessageGenerator(long messageId) {
-        messageText += String.valueOf(messageId).concat(getSmileSet());
+    MessageGenerator(long messageId) { // первый конструктор принимает только id сообщения
+        messageText += String.valueOf(messageId).concat(getSmileSet()); // преобразуем поле с помощью конкатенации
     }
 
-    MessageGenerator(String messageText, long messageId) {
-        if (messageId % 2 == 0) {
+    MessageGenerator(String messageText, long messageId) { // второй конструктор принимает два аргуменита
+        if (messageId % 2 == 0) { // проверяем messageId на чётность
+            /**
+             * Создаем экземпляр класса StringBuilder. Этот класс очень популярный и мы будем его часто использовать и в Java и в Kotlin
+             */
             StringBuilder modifiedText = new StringBuilder();
             for (int i = 0; i < messageText.length(); i++) {
                 if ((i + 1) % 2 != 0) {
-                    modifiedText.append(messageText.charAt(i));
+                    modifiedText.append(messageText.charAt(i)); // на объекте StringBuilder вызываем метод для конкатенации к имеющемуся charSequence новых char
                 }
             }
-            this.messageText = modifiedText.toString();
+            this.messageText = modifiedText.toString(); // обращаемся к messageText текущего конструктора и присваиваем ему новое значение
         } else {
-            StringBuilder modifiedText = new StringBuilder();
+            StringBuilder modifiedText = new StringBuilder(); // см. выше
             for (int i = 0; i < messageText.length(); i++) {
                 if ((i + 1) % 2 == 0) {
                     modifiedText.append(messageText.charAt(i));
@@ -27,24 +30,40 @@ public class MessageGenerator {
             }
             this.messageText = modifiedText.toString();
         }
-        this.messageText += messageId + getSmileSet();
+        this.messageText += messageId + getSmileSet(); // к преобразованной строке messageText конкатенируем messageId и возврат из метода getSmileSet()
     }
 
     public static String getMessage() {
         return messageText;
     }
 
-    private static String getRandomSmile() {
+    private static char getRandomSmile() { // метод просто возвращает рандомный смайл из строки
+        /**
+         * в этой строке демонстрация создания объекта (экземпляра класса String).
+         * String это класс с перегрузкой конструктора.
+         * Конечно же можно было переписать эту строку так: String smiles = "✅😁❤️🌟🎉🍕🎈";
+         */
         String smiles = new String("✅😁❤️🌟🎉🍕🎈");
-        Random random = new Random();
+        /**
+         * Для реализации рандомайзера обратимся к классу Random, для этого созданим его экземпляр и на его экземпляре вызовим метод nextInt.
+         * Кстати, класс Random тоже имеет перегрузку конструктора:  public Random(long seed)
+         */
+        Random random = new Random(120L);
+        /**
+         * с помощью экземпляра класса Random сгенерируем одно целое число.
+         * метод nextInt принмиает длину строки smiles и возвращает рандомное число от 0 до smiles.length()
+         */
         int randomIndex = random.nextInt(smiles.length());
-        return String.valueOf(smiles.charAt(randomIndex));
+        /**
+         * С помощью метода charAt класса String мы получаем рандомный char элемент
+         */
+        return smiles.charAt(randomIndex);
     }
 
-    private static String getSmileSet() {
-        Random repetitionNum = new Random();
-        StringBuilder smileSet = new StringBuilder();
-        for (int i = 0; i <= repetitionNum.nextInt(10); i++) {
+    private static String getSmileSet() { // этот метож вернет нам набор смайлов
+        Random repetitionNum = new Random(); // см. про класс Random выше
+        StringBuilder smileSet = new StringBuilder(); // см. про класс Random выше
+        for (int i = 0; i <= repetitionNum.nextInt(10); i++) { // генерируем набор смайлов в цикле
             smileSet.append(getRandomSmile());
         }
         return smileSet.toString();
