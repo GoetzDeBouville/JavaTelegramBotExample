7ed1cee5d7b17cef4c2ecf0d72fa0fabc63e6ba0
GoetzDeBouville <count.of.tuscan@gmail.com>
2024-03-16 21:07:13 +0300

    удаляет лишний коммент

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -9,8 +9,6 @@ import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
 
     public static void main(String[] args) {
